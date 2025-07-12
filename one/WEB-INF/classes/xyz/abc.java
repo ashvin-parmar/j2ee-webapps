0b176c36fceba721bd4f415af5293bafc961a9ca
Ashvin <ashwinparmar8181@gmail.com>
2025-07-12 16:52:23 +0530

    J2EE: Practice first example

--- one/WEB-INF/classes/xyz/abc.java
+++ one/WEB-INF/classes/xyz/abc.java
@@ -1,10 +1,9 @@
 package xyz;
-
-import javax.servlet.*;			//
-import javax.servlet.http.*; //HttpServlet,HttpServletRequest,HttpServletResponse
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.io.*;
 
-public class aaa extends HttpServlet
+public class abc extends HttpServlet
 {
 public void doGet(HttpServletRequest rq,HttpServletResponse rs)
 {
@@ -14,28 +13,25 @@ String nm=rq.getParameter("nm");
 String ct=rq.getParameter("ct");
 String gdr=rq.getParameter("gdr");
 
-System.out.println("Name: "+nm);
-System.out.println("City: "+ct);
-System.out.println("Gender: "+gdr);
 PrintWriter pw;
 pw=rs.getWriter();
 rs.setContentType("text/html");
 pw.println("<!DOCTYPE html>");
-pw.println("<hmtl lang='en'>");
+pw.println("<html>");
 pw.println("<head>");
 pw.println("<meta charset='utf-8'>");
 pw.println("<title>My First Web Application</title>");
-pw.println("</head>");
+pw.println("</head");
 pw.println("<body>");
+pw.println("<center>");
 pw.println("<h3>Data Saved</h3>");
-pw.println("<form action='/one/index.html'>");
-pw.println("<button type='submit'>OK</button>");
-pw.println("</form>");
+pw.println("<a href='/one/index.html'>OK</a>");
+pw.println("</center>");
 pw.println("</body>");
 pw.println("</html>");
-}catch(Exception exception)
+}catch(Exception e)
 {
-System.out.println(exception);
-}	
+System.out.println(e);
+}
 }
 }
