1cbd9415325674657826524353a79afce9a2856b
Ashvin <ashwinparmar8181@gmail.com>
2025-07-22 02:42:01 +0530

    J2EE: learning: Servlets DesignationView creation done! | JDBC code implemented to fetch data to table |

--- styleone/WEB-INF/classes/com/ashvin/hr/nexus/servlets/DesignationView.java
+++ styleone/WEB-INF/classes/com/ashvin/hr/nexus/servlets/DesignationView.java
@@ -20,9 +20,79 @@ designations=designationDAO.getAll();
 PrintWriter pw=response.getWriter();
 response.setContentType("text/html");
 
-//Code from DesignationViewTemplate.html
+pw.println("<!DOCTYPE HTML>");
+pw.println("<html lang='en'>");
+pw.println("<head>");
+pw.println("<meta charset='utf-8'>");
+pw.println("<title>Style one</title>");
+pw.println("</head>");
+pw.println("<body>");
+pw.println("<!-- Main content start here -->");
+pw.println("<div style='width:90hw;height:95vh;border:1px solid black'>");
+pw.println("<!-- header start here -->");
+pw.println("<div style='width:90hw;margin:5px;border:1px solid black'>");
+pw.println("<a href='/styleone/index.html'><img src='/styleone/images/hr_nexus_logo.png' style='width:30px;float:left'></a>");
+pw.println("<div style='margin:4px;font-size:15pt'>HR-Nexus</div>");
+pw.println("</div>");
+pw.println("<!-- header ends here -->");
+pw.println("<!-- middle content start here -->");
+pw.println("<div style='width:90hw;height:72vh;margin:5px;border:1px solid white'>");
+pw.println("<!-- left panel start here -->");
+pw.println("<div style='height:65vh;margin:5px;padding:5px;float:left;border:1px solid black'>");
+pw.println("<b>Designations</b><br>");
+pw.println("<a href='/styleone/EmployeeView' style='float:left'>Employees</a><br><br>");
+pw.println("<a href='/styleone/index.html'>Home</a>");
+pw.println("</div>");
+pw.println("<!-- left panel ends here -->");
+pw.println("<!-- right panel start here -->");
+pw.println("<div style='height:65vh;margin-left:110px;margin-right:5px;margin-bottom:5px;margin-top:5px;padding:5px;overflow:scroll;border:1px solid black'>");
+pw.println("<h2>Designations</h2>");
+pw.println("<table border='1'>");
+pw.println("<thead>");
+pw.println("<tr>");
+pw.println("<th colspan='4' style='text-align:right'><a href='/styleone/AddDesignation.html'>Add new designation</a></th>");
+pw.println("</tr>");
+pw.println("<tr>");
+pw.println("<th style='width:40px;text-align:center'>S.No.</th>");
+pw.println("<th style='width:200px;text-align:center'>Designation</th>");
+pw.println("<th style='width:80px;text-align:center'>Edit</th>");
+pw.println("<th style='width:80px;text-align:center'>Delete</th>");
+pw.println("</tr>");
+pw.println("</thead>");
+pw.println("<tbody>");
 
-
+int sno,i;
+DesignationDTO designationDTO;
+int code;
+String title;
+for(i=0,sno=0;i<designations.size();i++)
+{
+sno++;
+designationDTO=designations.get(i);
+code=designationDTO.getCode();
+title=designationDTO.getTitle();
+pw.println("<tr>");
+pw.println("<td style='text-align:right'>"+sno+".</td>");
+pw.println("<td>"+title+"</td>");
+pw.println("<td style='text-align:center'><a href='/styleone/EditDesignation?code="+code+"'>edit</a></td>");
+pw.println("<td style='text-align:center'><a href='/styleone/ConfirmDeleteDesignation?code="+code+"'>delete</a></td>");
+pw.println("</tr>");
+}
+pw.println("</tbody>");
+pw.println("</table>");
+pw.println("</div>");
+pw.println("<!-- right panel ends here -->");
+pw.println("</div>");
+pw.println("<!-- middle content ends here -->");
+pw.println("<!-- footer start here -->");
+pw.println("<div style='text-align:center;margin:5px;font-size:10pt;border:1px solid white'>");
+pw.println("&copy; HR-Nexus 2025");
+pw.println("</div>");
+pw.println("<!-- footer ends here -->");
+pw.println("</div>");
+pw.println("<!-- Main content ends here -->");
+pw.println("</body>");
+pw.println("</html>");
 }catch(DAOException daoException)
 {
 System.out.println(daoException.getMessage());		//505 error page[later on]
