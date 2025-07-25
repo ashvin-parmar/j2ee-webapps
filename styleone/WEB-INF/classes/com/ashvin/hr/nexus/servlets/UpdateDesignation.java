340206bce063364aebc9479cc14511c5b540b0ec
Ashvin <ashwinparmar8181@gmail.com>
2025-07-26 01:29:35 +0530

    J2EE: learning: styleone: Update Designation servlets added & modify EditDesignation

--- styleone/WEB-INF/classes/com/ashvin/hr/nexus/servlets/UpdateDesignation.java
+++ styleone/WEB-INF/classes/com/ashvin/hr/nexus/servlets/UpdateDesignation.java
@@ -4,30 +4,86 @@ import com.ashvin.hr.nexus.dl.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import java.util.*;
 
-public class EditDesignation extends HttpServlet
+public class UpdateDesignation extends HttpServlet
 {
 public void doGet(HttpServletRequest request,HttpServletResponse response)
 {
+//Declared and assigned such that use them in both -> try and catch blocks
 PrintWriter pw=null;
+String title="";
 int code=0;
 try
 {
-pw=response.getWriter();
+pw=response.getWriter();	
 response.setContentType("text/html");
+title=request.getParameter("title");
+if(title==null)
+{
+return ;
+}
 try
 {
 code=Integer.parseInt(request.getParameter("code"));
 }catch(NumberFormatException nfe)
 {
-System.out.println("Invalid code: "+code);	//removed after testing
 return ;
-//Maybe user feed some string or whatever
 }
-DesignationDAO designationDAO=new DesignationDAO();
-DesignationDTO designationDTO=designationDAO.getByCode(code);
-String title=designationDTO.getTitle();
-
+if(code<=0) 
+{
+return ;
+}
+DesignationDTO designation=new DesignationDTO();
+designation.setTitle(title);
+designation.setCode(code);
+(new DesignationDAO()).update(designation);
+pw.println("<!DOCTYPE HTML>");
+pw.println("<html lang='en'>");
+pw.println("<head>");
+pw.println("<meta charset='utf-8'>");
+pw.println("<title>Style one</title>");
+pw.println("</head>");
+pw.println("<body>");
+pw.println("<!-- Main content start here -->");
+pw.println("<div style='width:90hw;height:95vh;border:1px solid black'>");
+pw.println("<!-- header start here -->");
+pw.println("<div style='width:90hw;margin:5px;border:1px solid black'>");
+pw.println("<a href='/styleone/index.html'><img src='/styleone/images/hr_nexus_logo.png' style='width:30px;float:left'></a>");
+pw.println("<div style='margin:4px;font-size:15pt'>HR-Nexus</div>");
+pw.println("</div>");
+pw.println("<!-- header ends here -->");
+pw.println("<!-- middle content start here -->");
+pw.println("<div style='width:90hw;height:72vh;margin:5px;border:1px solid white'>");
+pw.println("<!-- left panel start here -->");
+pw.println("<div style='height:65vh;margin:5px;padding:5px;float:left;border:1px solid black'>");
+pw.println("<b>Designations</b><br>");
+pw.println("<a href='/styleone/employeeView' style='float:left'>Employees</a><br><br>");
+pw.println("<a href='/styleone/index.html'>Home</a>");
+pw.println("</div>");
+pw.println("<!-- left panel ends here -->");
+pw.println("<!-- right panel start here -->");
+pw.println("<div style='height:65vh;margin-left:110px;margin-right:5px;margin-bottom:5px;margin-top:5px;padding:5px;overflow:scroll;border:1px solid black'>");
+pw.println("<h2>Notification</h2>");
+pw.println("Designation Updated<br>");
+pw.println("<form action='/styleone/designationView'>");
+pw.println("<button type='submit'>OK</button>");
+pw.println("</form>");
+pw.println("</div>");
+pw.println("<!-- right panel ends here -->");
+pw.println("</div>");
+pw.println("<!-- middle content ends here -->");
+pw.println("<!-- footer start here -->");
+pw.println("<div style='text-align:center;margin:5px;font-size:10pt;border:1px solid white'>");
+pw.println("&copy; HR-Nexus 2025");
+pw.println("</div>");
+pw.println("<!-- footer ends here -->");
+pw.println("</div>");
+pw.println("<!-- Main content ends here -->");
+pw.println("</body>");
+pw.println("</html>");
+}catch(DAOException daoException)
+{
 pw.println("<!DOCTYPE HTML>");
 pw.println("<html lang='en'>");
 pw.println("<head>");
@@ -74,6 +130,8 @@ pw.println("<!-- left panel ends here -->");
 pw.println("<!-- right panel start here -->");
 pw.println("<div style='height:65vh;margin-left:110px;margin-right:5px;margin-bottom:5px;margin-top:5px;padding:5px;overflow:scroll;border:1px solid black'>");
 pw.println("<h2>Designation (Update Module)</h2>");
+pw.println("<div style='color:red'>"+daoException.getMessage()+"</div>");
+
 pw.println("<form method='post' action='/styleone/updateDesignation' onsubmit='return validateDesignation(this)'>");
 pw.println("Designation");
 pw.println("&nbsp;");
@@ -98,14 +156,13 @@ pw.println("<form id='cancelEditingForm' action='/styleone/designationView'>");
 pw.println("</form>");
 pw.println("</body>");
 pw.println("</html>");
-
-}catch(DAOException daoException)
-{
-System.out.println(daoException.getMessage());		//error page
 }catch(Exception exception)
 {
-System.out.println(exception.getMessage());	// removed after testing
+System.out.println(exception.getMessage());		//Removed after testing, exception mistaken by networking
 }
-
+}
+public void doPost(HttpServletRequest request,HttpServletResponse response)
+{
+doGet(request,response);
 }
 }
